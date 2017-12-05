dd4acd21ba4a24e4ce2f9b57f39d8889ceb3d6d1
cryptoeax <cryptoeax@mailinator.com>
2017-12-05 03:51:46 -0500

    Remove some dead code from utils.php

--- utils.php
+++ utils.php
@@ -19,27 +19,9 @@ function formatBTC( $value ) {
 
 }
 
-function startsWith( $haystack, $needle ) {
-
-  return $needle === "" || strpos( $haystack, $needle ) === 0;
-
-}
-
 function endsWith( $haystack, $needle ) {
 
   return $needle === "" || substr( $haystack, -strlen( $needle ) ) === $needle;
 
 }
 
-/*function logg( $message, $mail = false ) {
-
-  Database::log( $message );
-
-  echo date( "H:i:s" ) . ": " . $message . "\n";
-
-  if ( $mail ) {
-    sendmail( "LOG MESSAGE", date( "H:i:s" ) . ": " . $message );
-  }
-
-}
-*/
