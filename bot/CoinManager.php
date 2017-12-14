6a6fd1c5a8f6429c3835645ccd406d4fe481603c
cryptoeax <cryptoeax@mailinator.com>
2017-12-13 21:25:19 -0500

    Make the safety factor a bit more conservative

--- bot/CoinManager.php
+++ bot/CoinManager.php
@@ -276,8 +276,8 @@ class CoinManager {
     $safetyFactor = 0.01;
     if ( $coin == 'BTC' ) {
       $minXFER = Config::get( Config::MIN_BTC_XFER, Config::DEFAULT_MIN_BTC_XFER );
-      // Be twice more conservative with BTC, since it's our profits after all!
-      $safetyFactor /= 2;
+      // Be a bit more conservative with BTC, since it's our profits after all!
+      $safetyFactor /= 1.5;
     }
     foreach ( $exchanges as $exchange ) {
       // Allow max 1% of coin amount to be transfer fee:
