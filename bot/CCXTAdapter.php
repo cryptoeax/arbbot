64160ab6d4f11ca4c28c4337e5bdbcba7dc43dd5
cryptoeax <cryptoeax@mailinator.com>
2018-01-12 01:26:25 -0500

    Use amount

--- bot/CCXTAdapter.php
+++ bot/CCXTAdapter.php
@@ -191,7 +191,7 @@ abstract class CCXTAdapter extends Exchange {
         'type' => $type,
         'time' => floor( $row[ 'timestamp' ] / 1000 ), // timestamp is in milliseconds
         'rate' => floatval( $row[ 'price' ] ),
-        'amount' => floatval( $row[ 'filled' ] ),
+        'amount' => floatval( $row[ 'amount' ] ),
         'fee' => floatval( $row[ 'fee' ][ 'cost' ] ),
         'total' => floatval( $row[ 'cost' ] ),
       );
