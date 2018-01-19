956520a135b724c651c5d79222eb61a339b94f56
cryptoeax <cryptoeax@mailinator.com>
2018-01-19 01:17:35 -0500

    Revert "Show the live balance considering pending deposits in the web UI"
    We are doing this to eliminate the calls to refreshWallets() outside
    of the bot.
    
    This reverts commit 873f7dad9a93fad578ed166262a0d935efb45156.

--- web/WebDB.php
+++ web/WebDB.php
@@ -196,22 +196,12 @@ class WebDB {
     $extc = [ ];
     $exoc = [ ];
 
-    $exchangeMap = [ ];
-    $walletMap = [ ];
-
     $wallets = [ ];
     while ( $row = mysql_fetch_assoc( $result ) ) {
 
       $coin = $row[ 'coin' ];
       $exid = $row[ 'ID_exchange' ];
-      if ( !isset( $exchangeMap[ $exid ] ) ) {
-        $exchangeMap[ $exid ] = Exchange::createFromID( $exid );
-        $exchangeMap[ $exid ]->refreshWallets();
-        $walletMap[ $exid ] = $exchangeMap[ $exid ]->getWalletsConsideringPendingDeposits();
-      }
-
-      // Will be 0 if $coin doesn't exist in our wallets!
-      $balance = @floatval( @$walletMap[ $exid ][ $coin ] );
+      $balance = $row[ 'balance' ];
 
       if ( key_exists( $exid, $extc ) === false ) {
         $extc[ $exid ] = intval( self::getTradeCount( $exid ) );
@@ -219,7 +209,7 @@ class WebDB {
       }
 
       $age = time() - $row[ 'created' ];
-      $wallets[ $coin ][ $exid ][ 'balance' ] = $balance;
+      $wallets[ $coin ][ $exid ][ 'balance' ] = floatval( $balance );
       $wallets[ $coin ][ $exid ][ 'balance_diff' ] = floatval( formatBTC( $row[ 'desired_balance' ] - $balance ) );
       $wallets[ $coin ][ $exid ][ 'opportunities' ] = intval( $row[ 'uses' ] );
       $wallets[ $coin ][ $exid ][ 'change' ] = floatval( $balance  - self::getHistoricBalance( $coin, $exid ) );
