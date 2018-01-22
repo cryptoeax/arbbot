cryptoeax/arbbot

commit 27ef12c151e492d8c950e7fc9faf6f49ba88be63

Check exchange pairs in both directions

bot/Arbitrator.php
@@ -115,7 +115,8 @@ class Arbitrator {
 
     foreach ( $slicedPairs as $pair ) {
 
-      if ( $this->checkPairAt( $pair, $x1, $x2 ) ) {
+      if ( $this->checkPairAt( $pair, $x1, $x2 ) ||
+           $this->checkPairAt( $pair, $x2, $x1 ) ) {
         return true;
       }
     }
@@ -174,7 +175,7 @@ class Arbitrator {
       return;
     }
 
-    logg( "Checking $pair..." );
+    logg( "Checking $pair at " . $x1->getName() . " -> " . $x2->getName() . "..." );
 
     // Measure time for orderbook checks. Sometimes it takes 20+ seconds and could result in a failed trade
     $timeout = time() + 10;

commit ccae89858933bb8350cfb6d2b4c9d2ba482d2c87

Correctly check for the arbitrage opportunities in both directions
Fixes #118, a regression from 27ef12c.

bot/Arbitrator.php
@@ -88,7 +88,8 @@ class Arbitrator {
 
     foreach ( $this->exchangePairs as $exchangePair ) {
 
-      if ( $this->checkOpportunitiesAt( $exchangePair[ 0 ], $exchangePair[ 1 ] ) ) {
+      if ( $this->checkOpportunitiesAt( $exchangePair[ 0 ], $exchangePair[ 1 ] ) ||
+           $this->checkOpportunitiesAt( $exchangePair[ 1 ], $exchangePair[ 0 ] ) ) {
         // Trade happened, restart...
         $this->tradeHappened = true;
         return;
@@ -112,8 +113,7 @@ class Arbitrator {
 
     foreach ( $slicedPairs as $pair ) {
 
-      if ( $this->checkPairAt( $pair, $x1, $x2 ) ||
-           $this->checkPairAt( $pair, $x2, $x1 ) ) {
+      if ( $this->checkPairAt( $pair, $x1, $x2 ) ) {
         return true;
       }
     }