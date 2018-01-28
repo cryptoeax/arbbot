04e8e9c8b59fab0ecca79ba04d2f4dd79c4ddfb4
cryptoeax <cryptoeax@mailinator.com>
2018-01-28 22:25:05 +0000

    More various fixes

--- docker/bot/wait-for-db.sh
+++ docker/bot/wait-for-db.sh
@@ -6,7 +6,7 @@ sleep 10
 waitForTable() {
     table=$1;
     db=$2
-    count=$(mysql -N -s -u root -p$3 -h db -e \
+    count=$(mysql -N -s -u root '-p$3' -h db -e \
         "select count(*) from information_schema.tables where \
             table_schema='${db}' and table_name='${table}';" 2>/dev/null)
     while [ "$count" -eq "0" ]; do
